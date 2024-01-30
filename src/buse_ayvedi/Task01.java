bdf358d98c83a3c6c4d43e213e6614149c6ce553
buseayvedi <buseayvedi@gmail.com>
2024-01-30 13:22:02 -0500

    add Task1.java

---
+++ src/buse_ayvedi/Task01.java
@@ -0,0 +1,23 @@
+package buse_ayvedi;
+
+public class Task01 {
+    public static void main(String[] args) {
+        System.out.println(oddOrEven(5));
+        System.out.println(oddOrEven(6));
+    }
+    public static String oddOrEven(int n){
+        if (n % 2 == 0){
+            return "even";
+        }else{
+            return "odd";
+        }
+    }
+
+}
+/*
+Numbers -- odd & even
+Write a method to identify whether a given number is even or odd.
+EX:
+identify(5) --> "Odd"
+identify(6) --> "Even"
+ */
