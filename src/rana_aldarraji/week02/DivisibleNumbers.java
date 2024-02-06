60046489322691dcec10e4dc18d7cadced604d82
ranosh <raldarra@gmail.com>
2024-02-05 21:56:06 -0500

    Added code in DivisibleNumbers.java class

--- src/rana_aldarraji/week02/DivisibleNumbers.java
+++ src/rana_aldarraji/week02/DivisibleNumbers.java
@@ -3,7 +3,24 @@ package rana_aldarraji.week02;
 public class DivisibleNumbers {
     public static void main(String[] args) {
 
-
+        System.out.print("Divisible by 15: ");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.print("\nDivisible by 5: ");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.print("\nDivisible by 3: ");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
 /*
