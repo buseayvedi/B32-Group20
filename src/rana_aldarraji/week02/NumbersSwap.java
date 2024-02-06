39226da8ca416d918c53071f4e2603e1094a0ddc
ranosh <raldarra@gmail.com>
2024-02-05 20:39:09 -0500

    Created SwapNumbers.java class

---
+++ src/rana_aldarraji/week02/NumbersSwap.java
@@ -0,0 +1,26 @@
+package rana_aldarraji.week02;
+
+public class NumbersSwap {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5 ;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        System.out.println("---------------");
+
+        a = a - b; // a = 5
+        b = b + a; // b = 10
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+
+    }
+}
+/*
+Swap two variable's values without using a third variable.
+ */
