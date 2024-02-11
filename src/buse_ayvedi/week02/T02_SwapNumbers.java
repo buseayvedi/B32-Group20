f7d02267df8f29dba91280d0a8e032b6979ee3b4
buseayvedi <buseayvedi@gmail.com>
2024-02-10 20:38:41 -0500

    add week2 tasks

---
+++ src/buse_ayvedi/week02/T02_SwapNumbers.java
@@ -0,0 +1,22 @@
+package buse_ayvedi.week02;
+
+import java.util.Arrays;
+
+public class T02_SwapNumbers {
+
+    public static void main(String[] args) {
+
+        swapNumbers(1,2);
+
+    }
+
+    public static void swapNumbers(int n1, int n2){
+        System.out.println(Arrays.asList(n2,n1));
+    }
+
+}
+/*
+1.Numbers -- Swap Numbers.
+Swap two variable values without using а third variable.
+
+ */
