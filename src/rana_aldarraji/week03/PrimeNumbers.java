810842f34291f4fabaf68ac11fb09d91b022b642
ranosh <raldarra@gmail.com>
2024-02-15 20:38:08 -0500

    Added code into PrimeNumbers.java

--- src/rana_aldarraji/week03/PrimeNumbers.java
+++ src/rana_aldarraji/week03/PrimeNumbers.java
@@ -1,10 +1,36 @@
 package rana_aldarraji.week03;
 
+import java.util.Scanner;
+
 public class PrimeNumbers {
 
 
+    public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.println("Enter a number: ");
+        System.out.println("isPrime: " + isPrime(scan.nextInt()));
+
+    }
+
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i < num; i++) { // the smallest prime number is 2, it's also the only even prime number
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
+
 /*
 Write a method that can check if a number is prime or not
  */
