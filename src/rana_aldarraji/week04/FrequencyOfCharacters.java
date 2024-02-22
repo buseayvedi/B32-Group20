d5e033922524b4e87072f6dc0bafe9d909cf2f63
ranosh <raldarra@gmail.com>
2024-02-22 18:41:22 -0500

    Added code in FrequencyOfCharacters.java

--- src/rana_aldarraji/week04/FrequencyOfCharacters.java
+++ src/rana_aldarraji/week04/FrequencyOfCharacters.java
@@ -1,4 +1,38 @@
 package rana_aldarraji.week04;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class FrequencyOfCharacters {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your String:");
+        System.out.println(countLetters(in.next()));
+    }
+
+    public static String countLetters(String str) {
+
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (!result.contains("" + str.charAt(i))) {
+                int count = 0; // reset every iteration of the i loop
+
+                for (int j = 0; j < str.length(); j++) {
+                    if (str.charAt(i) == str.charAt(j)) {
+                        count++;
+                    }
+                }
+
+                result += "" + str.charAt(i) + count;
+
+            }
+        }
+        return result;
+    }
 }
+/*
+Write a return method that can find the frequency of characters
+"AAABBCDD"--> A3B2C1D2
+ */
