24137133acce33b6950b4ce64cf473b2ef15273f
Rana <raldarra@gmail.com>
2024-02-28 23:52:29 -0500

    Added code in String_UniqueCharacters.java

--- src/rana_aldarraji/week05/String_UniqueCharacters.java
+++ src/rana_aldarraji/week05/String_UniqueCharacters.java
@@ -1,4 +1,33 @@
 package rana_aldarraji.week05;
 
+import java.util.Scanner;
+
 public class String_UniqueCharacters {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a String:");
+        System.out.println(uniqueCharacters(scan.nextLine()));
+
+    }
+
+    public static String uniqueCharacters(String str) {
+
+
+        String unique = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (str.indexOf(ch) == str.lastIndexOf(ch)) { // --> if the character appeared from left-right(str.indexOf) equals the times the character appeared from right-left(str.lastIndexOf) then it is unique.
+                unique += ch;
+            }
+        }
+        return unique;
+    }
+
 }
+/*
+Write a return  method that can find the unique characters from the String
+Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";
+ */
