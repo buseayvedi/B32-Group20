3ec84148bdd40a94be19786dd7b8b33016f84728
ranosh <raldarra@gmail.com>
2024-03-12 05:06:24 -0400

    Added code to MoveZero.java class

--- src/rana_aldarraji/week08/MoveZero.java
+++ src/rana_aldarraji/week08/MoveZero.java
@@ -1,7 +1,28 @@
 package rana_aldarraji.week08;
 
-public class MoveZero {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class MoveZero {
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 2, 0, 3, 0, 4, 0};
+
+        // Two-pointer approach
+        int nums = 0;
+
+        for (int i = 0; i < arr.length; i++) { // {1, 0, 2, 0, 3, 0, 4, 0};
+            if (arr[i] != 0) { // true (1 != 0)
+                // If the element is non-zero, swap it with the element at nonZeroIndex
+                int containerForInt = arr[i]; //  temp = 1
+                arr[i] = arr[nums]; // arr[i] = 1 --> arr[nums -was-> 0 --then became--> 1]
+                arr[nums] = containerForInt; // arr[nums] = 1 --> now temp = 1
+                nums++; // num ++ 1
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+    }
 }
 /*
 Array - Move Zeros to the End
