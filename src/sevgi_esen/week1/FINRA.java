eedcf8aa79cfa58f08a65a45f8ce5ee90f5fbbcd
SevgiEsen <sevgi-esen@outlook.com>
2024-03-15 13:08:52 -0400

    Week 4 tasks

--- src/sevgi_esen/week1/FINRA.java
+++ src/sevgi_esen/week1/FINRA.java
@@ -1,11 +1,64 @@
 package sevgi_esen.week1;
 
+import java.util.Arrays;
+
 public class FINRA {
     public static void main(String[] args) {
+        FINRA_1();
+        FINRA_2();
+        FINRA_3();
+    }
 
-//Hello world
+    public static void FINRA_1() {
+        String result = "";
+        for(int i=1; i <= 30; i++) {
+            if(i % 5==0 && i %3 ==0)
+                result += "FINRA ";
+            else if(i%5 == 0)
+                result += "RA ";
+            else if(i%3==0)
+                result+="FIN ";
+            else
+                result += i+" ";
+        }
+        System.out.println(result);
+    }
+
+    public static void FINRA_2() {
+        String result = "";
+        for(int i=1; i <= 30; i++) {
+            result += (i % 5 ==0 && i %3 ==0)? "FINRA " : (i%5 == 0) ? "RA "
+                    :(i%3 == 0) ? "FIN " : i+" ";
+        }
+        System.out.println(result);
+    }
+
+    public static void FINRA_3() {
+        String[] myarr= new String[30];
+
+        for( int i=0; i <= 29; i++ )
+            myarr[i] = ""+(i+1);
+
+        for(int j=0; j<myarr.length; j++)
+            if(Integer.valueOf(myarr[j])%3==0 && new Integer(myarr[j])%5==0)
+                myarr[j]="FINRA";
+            else if (Integer.valueOf(myarr[j])%3==0)
+                myarr[j]="FIN";
+            else if (Integer.valueOf(myarr[j])%5==0)
+                myarr[j]="RA";
+
+        System.out.println(Arrays.deepToString(myarr));
+    }
+
 
 
 
     }
-}
+
+
+/*
+Write a method which prints out the numbers from 1 to 30 but for numbers which
+are a multiple of 3print "FIN" instead of the number and for numbers which are a
+multiple of 5, print "RA" instead of the number. for numbers which are a multiple of
+both 3 and 5, print "FINRA" instead of the number.
+ */
